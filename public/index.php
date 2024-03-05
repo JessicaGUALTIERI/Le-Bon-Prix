fc6ee5b58e030c6938ab5c57d603f70a3ae1895e
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-03-05 13:54:38 +0100

    Ajout de la page 'rules'

--- public/index.php
+++ public/index.php
@@ -1,38 +1,26 @@
 <?php
+    session_start();
     require('functions.php');
     require('header.html');
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le Bon Prix</title>
-    <link href=".//css/main.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
-</head>
-<body>
-    <div class="container">
-        <div class="item">
-            <img src="<?php displayItemImg() ?>" alt="illustration de l'item"/>
-            <div class="desc">
-                <p class="title">
-                    <?php displayItemTitle(); ?>
-                    <!--Prix = <?php displayItemPrice(); ?>-->
-                </p>
-                <form action="" method="post">
-                    <input type="number" name="nombreUtilisateur">
-                    <button type="submit" class="btn btn-dark">Deviner</button>
-                <form>
-            </div>
+<div class="container">
+    <div class="item">
+        <img src="<?php displayItemImg() ?>" alt="illustration de l'item"/>
+        <div class="desc">
+            <p class="title">
+                <?php displayItemTitle(); ?>
+                <!--Prix = <?php displayItemPrice(); ?>-->
+            </p>
+            <form action="" method="post">
+                <input type="number" name="nombreUtilisateur">
+                <button type="submit" class="btn btn-dark">Deviner</button>
+            <form>
         </div>
     </div>
-    <script src="js/bundle.js"></script>
-</body>
-</html>
+</div>
+<script src="js/bundle.js"></script>
+
 
 <?php
     require('footer.html');
---
+++ public/rules.php
@@ -0,0 +1,13 @@
+<?php
+    require('functions.php');
+    require('header.html');
+?>
+
+<h1>Règles du jeu</h1>
+
+
+<script src="js/bundle.js"></script>
+
+<?php
+    require('footer.html');
+?>
